b69697d0b121a794d912d76e9edb08fdb94c7b2a
22f3000646 <22f3000646@ds.study.iitm.ac.in>
2024-07-13 01:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    user = "John Doe"
-    return render_template('index.html', user=user)
-
-if __name__ == '__main__':
-    app.run(debug=True)
