06daa2d8c545347fc56a45961648efe9a4a649f3
Akshay <22f3000646@ds.study.iitm.ac.in>
2024-07-14 17:36:40 +0530

    app.py file added

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/admin")
+def Admin():
+    return render_template("Adminlogin.html")
+@app.route("/influencer")
+def influencer():
+    return render_template("influencer_login.html")
 
+@app.route("/sponsor")
+def sponsor():
+    return render_template("sponsor login.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
