90df93196465e34c38124ab7a3ec9af8a56f88bb
Akshay <22f3000646@ds.study.iitm.ac.in>
2024-08-03 15:23:44 +0530

    commit

--- The-Influence-Mart-app/__init__.py
+++ The-Influence-Mart-app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from admin import admin_bp
-from influencer import influencer_bp
-from sponsors import sponsor_bp
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# from admin import admin_bp
+# from influencer import influencer_bp
+# from sponsors import sponsor_bp
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -11,11 +11,10 @@ def home():
 app.register_blueprint(admin_bp,url_prefix='/admin')
 app.register_blueprint(influencer_bp,url_prefix='/influencer')
 app.register_blueprint(sponsor_bp,url_prefix='/sponsor')
-app.config.from_object('config.Config')  # Load configuration from config.py
+# app.config.from_object('config.Config')  # Load configuration from config.py
 # db = SQLAlchemy(app)
 # login_manager = LoginManager(app)
 # login_manager.login_view = 'login'
-
 
 
 if __name__=='__main__':
